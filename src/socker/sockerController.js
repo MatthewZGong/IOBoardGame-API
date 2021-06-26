79b667971226a7d2e1d52ddb5b9f9f74c9387419
MatthewZGong <matthewzgong@gmail.com>
2021-06-25 22:30:33 -0700

    socket test: sending init cords

--- src/socker/sockerController.js
+++ src/socker/sockerController.js
@@ -1,4 +1,5 @@
 import { Server } from 'socket.io';
+import { MapClient } from "../game/hexaboard"
 const app = app => {
     const io = new Server(app, {
         cors: {
@@ -9,8 +10,17 @@ const app = app => {
     console.log('connected')
     io.on('connection', (socket) => {
         console.log('user ' + socket.id + ' connected');
-    });
-      
+        var map = new MapClient(); 
+        map.hexagon(10); 
+        var cords = [] 
+        for(const hex of map){
+            cords.push(map.hexToPixel(hex))
+        }
+        socket.emit("init board", cords)
+        // socket.on("click", (cord) =>{
+        //     map.pixelToHex(cord)
+        // }); 
+    }); 
     return io;
 }
 export default app;
