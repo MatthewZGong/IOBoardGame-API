a53b79ce4fd1c854c6192eec4f8267d3f57d50f1
MatthewZGong <matthewzgong@gmail.com>
2021-06-28 17:02:05 -0700

    env variables and rooms udpate

--- src/game/gameClient.js
+++ src/game/gameClient.js
@@ -1,3 +1,19 @@
-class GameClient{
-    
+import { MapClient } from "./hexaboard.js"
+
+export default class GameClient{
+    constructor(){
+        this.characters = [] 
+        this.map = new MapClient() 
+
+    }
+    gameStart(roomID){
+        this.map.hexagon(5)
+        return;
+    }
+    getHexMap(){
+        return this.map.getHexMap();
+    }
+    getPath(center, distance){
+        return this.map.bfs(center, distance);
+    }
 }
